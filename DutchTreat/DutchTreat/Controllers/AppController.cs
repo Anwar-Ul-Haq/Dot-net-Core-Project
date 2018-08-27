09df321507ed899f4a47289c24681fa1ff773ae0
Anwar Ul-Haq <aulhaq6@gmail.com>
2018-08-27 20:08:41 +0100

    Added initial .net core project with bootstrap, logging,razor pages,jquery validation etc

---
+++ DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        [HttpGet("contact")]
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+           
+
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+              _mailService.SendMessage("aulhaq6@gmail.com",model.Subject,$"From:{model.Name} - {model.Email},Message:{model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+               
+            }
+
+            return View();
+
+        }
+
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+    }
+}
