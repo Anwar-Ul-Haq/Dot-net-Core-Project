7636606faf8060db66d628ed78a72597a055fac7
Anwar Ul-Haq <aulhaq6@gmail.com>
2018-08-30 22:37:41 +0100

    Added EF with  sample classes  and migration

---
+++ DutchTreat/DutchTreat/Data/DutchContex.cs
@@ -0,0 +1,17 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DutchTreat.Data
+{
+    public class DutchContex :DbContext
+    {
+
+        public DutchContex(DbContextOptions<DutchContex> options) :base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
